bdb80fd6334ebbb505aaeb6638b002fbd92cde25
Thomas Kercheval <spacekattpoispin@gmail.com>
2018-12-02 16:11:39 -0800

    Add scaffolding for webserver

---
+++ src/main.py
@@ -0,0 +1,48 @@
+'''
+Runs the webserver.
+'''
+
+# External dependencies
+# import aiofiles
+import asyncio
+# import json
+
+from aiohttp import web
+
+# built-in dependencies
+# import traceback
+# import io
+
+# from gzip import GzipFile
+# from urllib.parse import urlparse
+# from os import path
+
+
+ROUTES = web.RouteTableDef()
+
+
+@ROUTES.get('/')
+async def root_handle(req):
+    '''
+    Tells the malcontent to go root themselves off our lawn.
+    '''
+    return web.Response(status=200, body='Stuff goes here')
+
+
+async def init_app():
+    '''
+    Initialize the database, then application server
+    '''
+    app = web.Application()
+
+    app.add_routes(ROUTES)
+
+    return app
+
+
+LOOP = asyncio.get_event_loop()
+APP = LOOP.run_until_complete(init_app())
+
+
+if __name__ == '__main__':
+    web.run_app(APP, host='0.0.0.0:8080')
